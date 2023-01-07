da0b73fd37d0a2d4a704634f35749235024f64d0
Quentin Buache <quentin.buache@bluewin.ch>
2023-01-07 18:17:09 +0100

    Affiche un message s'il n'y a rien à afficher dans la liste des potagers

--- resources/views/potagers/index.blade.php
+++ resources/views/potagers/index.blade.php
@@ -2,13 +2,17 @@
     <div class="col-lg-8 mx-auto">
         <x-page>
             <div class="list-group">
-                @foreach ($potagers as $potager)
-                    <a class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
-                        href="{{ url("potagers/{$potager->id}") }}">
+                @forelse($potagers as $potager)
+                    <a
+                        class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
+                        href="{{ url("potagers/{$potager->id}") }}"
+                    >
                         <span>{{ $potager->name }}</span>
                         <small class="text-muted">{{ $potager->jardin->name }}</small>
                     </a>
-                @endforeach
+                @empty
+                    <x-alert>Il n'y a pas de potager</x-alert>
+                @endforelse
             </div>
         </x-page>
     </div>
