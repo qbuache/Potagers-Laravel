4540401537c58604d3161218cde6a3e4a3866883
Quentin Buache <quentin.buache@bluewin.ch>
2023-01-08 18:12:09 +0100

    Ajoute la description d'un jardin en dessous du titre de la page

--- resources/views/jardins/show.blade.php
+++ resources/views/jardins/show.blade.php
@@ -1,5 +1,12 @@
 <x-app-layout>
     <x-page>
+        @if ($jardin->description)
+            <x-slot name="pageSubText">
+                <span class="text-muted">
+                    {{ $jardin->description }}
+                </span>
+            </x-slot>
+        @endif
         <x-btn-group>
             @can(Permissions::DELETE_JARDINS)
                 <x-button
