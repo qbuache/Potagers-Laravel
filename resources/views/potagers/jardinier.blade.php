9a1e9dea9d8af468de84bc9ebedb73a7b1dd6235
Quentin Buache <quentin.buache@unil.ch>
2023-01-09 10:42:21 +0100

    Corrige l'utilisation du composant form dans la définition du jardinier d'un potager

--- resources/views/potagers/jardinier.blade.php
+++ resources/views/potagers/jardinier.blade.php
@@ -1,12 +1,14 @@
+@php
+    $element = $potager;
+    $route = route('potagers.update_jardinier', $potager->id);
+@endphp
 <x-app-layout>
     <div class="col-lg-10 mx-auto">
         <x-page>
-            <form
-                action="{{ route('potagers.update_jardinier', $potager->id) }}"
-                method="POST"
+            <x-form.form
+                :element="$element"
+                :route="$route"
             >
-                @csrf
-                @method('PATCH')
                 @if ($users->isNotEmpty())
                     <x-form.select
                         name="user_id"
@@ -18,7 +20,7 @@
                     <x-alert>Il n'y a pas de jardinier</x-alert>
                 @endif
                 <x-form.submit />
-            </form>
+            </x-form.form>
         </x-page>
     </div>
 </x-app-layout>
