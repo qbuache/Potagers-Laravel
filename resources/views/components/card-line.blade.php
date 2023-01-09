6f8f2752a8c77d07240bf9231ec61fd5d0c2668c
Quentin Buache <quentin.buache@unil.ch>
2023-01-09 13:56:42 +0100

    Corrige l'affichage d'une card-line

--- resources/views/components/card-line.blade.php
+++ resources/views/components/card-line.blade.php
@@ -1,9 +1,6 @@
 @props(['name', 'slot' => '-'])
 
 <div class="card-line">
-    <label class="text-muted">
-        {{ __("messages.label.{$name}") }}
-        <x-slot name="suppText"></x-slot>
-    </label>
+    <label class="text-muted">{{ __("messages.label.{$name}") }}<x-slot name="suppText"></x-slot></label>
     <span>{{ $slot }}</span>
 </div>
