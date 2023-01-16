34c623ec832621af9651721b9844efea5584ac93
Quentin Buache <quentin.buache@bluewin.ch>
2023-01-16 19:37:46 +0100

    Modifie le layout de la page des potagers afin de mettre les filtres à gauche

--- resources/views/potagers/index.blade.php
+++ resources/views/potagers/index.blade.php
@@ -1,81 +1,84 @@
 <x-app-layout>
-    <x-page>
-        <input
-            class="form-control"
-            id="searchBox"
-            type="search"
-            placeholder="Rechercher..."
-        >
-        <table class="table table-sm table-round table-boxed table-bordered my-3">
-            <tbody>
-                <tr>
-                    <td>
-                        <div class="d-flex justify-content-center">
+    <div class="row">
+        <div class="col-lg-3">
+            <x-page
+                class="sticky"
+                noHeader
+            >
+                <input
+                    class="form-control"
+                    id="searchBox"
+                    type="search"
+                    placeholder="Rechercher..."
+                />
+                <ul class="mt-3 mb-0 list-unstyled">
+                    <li>
+                        <x-form.simple.radio
+                            class="size"
+                            id="sizeAll"
+                            name="size"
+                            value=""
+                            :checked="true"
+                            noMb
+                        >Tous</x-form.simple.radio>
+                    </li>
+                    @foreach ($sizes as $size => $count)
+                        <li>
                             <x-form.simple.radio
                                 class="size"
-                                id="sizeAll"
+                                id="size{{ $size }}"
                                 name="size"
-                                value=""
-                                :checked="true"
+                                value="{{ $size }}"
                                 noMb
-                            >Tous</x-form.simple.radio>
-                        </div>
-                    </td>
-                    @foreach ($sizes as $size => $count)
-                        <td>
-                            <div class="d-flex justify-content-center">
-                                <x-form.simple.radio
-                                    class="size"
-                                    id="size{{ $size }}"
-                                    name="size"
-                                    value="{{ $size }}"
-                                    noMb
-                                >
-                                    <x-sqm>{{ $count }}x{{ $size }}</x-sqm>
-                                </x-form.simple.radio>
-                            </div>
-                        </td>
-                    @endforeach
-                </tr>
-            </tbody>
-        </table>
-        <div
-            class="rounded p-0"
-            id="potagers"
-        >
-            <div class="list-group list-group-flush border border-bottom-0 overflow-hidden rounded">
-                @forelse($potagers as $potager)
-                    <a
-                        class="potager list-group-item list-group-item-action border-bottom"
-                        data-size="{{ $potager->size }}"
-                        href="{{ url("potagers/{$potager->id}") }}"
-                    >
-                        <div class="row">
-                            <div class="col-lg-3">{{ $potager->name }}</div>
-                            <small class="col-lg-2 text-lg-center text-muted">
-                                <x-sqm>{{ $potager->size }}</x-sqm>
-                            </small>
-                            <small
-                                class="col-lg-2 text-lg-center {{ $potager->state != 'ok' ? 'text-warning' : 'text-muted' }}"
                             >
-                                {{ $potager->state_text }}
-                            </small>
-                            <small class="col-lg-2 text-lg-center text-muted">
-                                {{ !empty($potager->jardinier) ? 'Attribué' : 'Libre' }}
-                            </small>
-                            <small class="col-lg-3 text-lg-end text-muted">{{ $potager->jardin->name }}</small>
-                        </div>
-                    </a>
-                @empty
-                    <x-alert>Il n'y a pas de potager</x-alert>
-                @endforelse
-            </div>
+                                <x-sqm>{{ $size }}</x-sqm>
+                            </x-form.simple.radio>
+                        </li>
+                    @endforeach
+                </ul>
+            </x-page>
         </div>
-        <x-alert
-            id="noResult"
-            style="display:none"
-        >Pas de résultat</x-alert>
-    </x-page>
+        <div class="col-lg-9">
+            <x-page>
+                <div
+                    class="rounded p-0"
+                    id="potagers"
+                >
+                    <div class="list-group list-group-flush border border-bottom-0 overflow-hidden rounded">
+                        @forelse($potagers as $potager)
+                            <a
+                                class="potager list-group-item list-group-item-action border-bottom"
+                                data-size="{{ $potager->size }}"
+                                href="{{ url("potagers/{$potager->id}") }}"
+                            >
+                                <div class="row">
+                                    <div class="col-lg-3">{{ $potager->name }}</div>
+                                    <small class="col-lg-2 text-lg-center text-muted">
+                                        <x-sqm>{{ $potager->size }}</x-sqm>
+                                    </small>
+                                    <small
+                                        class="col-lg-2 text-lg-center {{ $potager->state != 'ok' ? 'text-warning' : 'text-muted' }}"
+                                    >
+                                        {{ $potager->state_text }}
+                                    </small>
+                                    <small class="col-lg-2 text-lg-center text-muted">
+                                        {{ !empty($potager->jardinier) ? 'Attribué' : 'Libre' }}
+                                    </small>
+                                    <small class="col-lg-3 text-lg-end text-muted">{{ $potager->jardin->name }}</small>
+                                </div>
+                            </a>
+                        @empty
+                            <x-alert>Il n'y a pas de potager</x-alert>
+                        @endforelse
+                    </div>
+                </div>
+                <x-alert
+                    id="noResult"
+                    style="display:none"
+                >Pas de résultat</x-alert>
+            </x-page>
+        </div>
+    </div>
 </x-app-layout>
 <script
     src="{{ asset('assets/js/potagers.index.js') }}"
