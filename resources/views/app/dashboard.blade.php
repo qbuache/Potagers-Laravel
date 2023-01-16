1b42fa110df327144f7735b8d7e4bb36d99e5558
Quentin Buache <quentin.buache@unil.ch>
2023-01-16 11:23:21 +0100

    met à jour l'aspect de la page d'accueil

--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -21,49 +21,41 @@
     ];
 @endphp
 <x-app-layout>
-    <x-page>
-        <div class="container">
-            <div class="row row-cols-1 row-cols-lg-4 gap-3">
-                @can(Permissions::READ_JARDINS)
-                    <div class="card card-body h-100">
-                        <x-link
-                            class="text-start text-lg-center stretched-link"
-                            href="jardins"
-                            fa="seedling"
-                            size="nm"
-                        >Jardins</x-link>
-                        <div class="mt-3 text-center">
-                            Nombre de jardins : {{ $countJardins }}
-                        </div>
-                    </div>
-                @endcan
-                @can(Permissions::READ_USERS)
-                    <div class="card card-body h-100">
-                        <x-link
-                            class="text-start text-lg-center stretched-link"
-                            href="users"
-                            fa="users"
-                            size="nm"
-                        >Jardiniers</x-link>
-                        <div class="mt-3 text-center">
-                            Nombre de jardiniers : {{ $countJardiniers }}
-                        </div>
-                    </div>
-                @endcan
-                @can(Permissions::READ_POTAGERS)
-                    <div class="card card-body h-100">
-                        <x-link
-                            class="text-start text-lg-center stretched-link"
-                            href="potagers"
-                            fa="carrot"
-                            size="nm"
-                        >Potagers</x-link>
-                        <div class="mt-3 text-center">
-                            Nombre de potagers : {{ $countPotagers }}
-                        </div>
-                    </div>
-                @endcan
-            </div>
+    <x-page title="{{ __('messages.text.dashboard') }}">
+        <div class="d-flex flex-column flex-lg-row gap-3">
+            @can(Permissions::READ_JARDINS)
+                <x-link
+                    class="flex-grow-1 text-start text-lg-center"
+                    href="jardins"
+                    fa="seedling"
+                    size="nm"
+                >
+                    <span class="text-custom">{{ $countJardins }}</span>
+                    <span>Jardins</span>
+                </x-link>
+            @endcan
+            @can(Permissions::READ_USERS)
+                <x-link
+                    class="flex-grow-1 text-start text-lg-center"
+                    href="users"
+                    fa="users"
+                    size="nm"
+                >
+                    <span class="text-custom">{{ $countJardiniers }}</span>
+                    <span>Jardiniers</span>
+                </x-link>
+            @endcan
+            @can(Permissions::READ_POTAGERS)
+                <x-link
+                    class="flex-grow-1 text-start text-lg-center"
+                    href="potagers"
+                    fa="carrot"
+                    size="nm"
+                >
+                    <span class="text-custom">{{ $countPotagers }}</span>
+                    <span>Potagers</span>
+                </x-link>
+            @endcan
         </div>
     </x-page>
 </x-app-layout>
